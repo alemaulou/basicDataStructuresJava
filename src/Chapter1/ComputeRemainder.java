1608cd7ca33652fa0a0a98c6557ec761fce0ff83
alemaulou <alemau@uw.edu>
2019-02-03 22:38:21 -0800

    Practicing Data Structures

---
+++ src/Chapter1/ComputeRemainder.java
@@ -0,0 +1,26 @@
+package Chapter1;
+
+public class ComputeRemainder {
+    public static long computeRemainder(long base, long power, long divisor) {
+        long baseRaisedToPower = 1;
+        
+        for(long i = 1; i <= power; i++) {
+            baseRaisedToPower *= base;
+        }
+        return baseRaisedToPower % divisor;
+    }
+
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        long ans = 0;
+        
+        for(int i = 0; i < 1_000_000_000; i++) {
+            ans = computeRemainder(2, 1000, 7);
+        }
+        
+        long endTime = System.currentTimeMillis();
+        System.out.println(ans);
+        System.out.println(endTime-startTime);
+    }
+}   
+
